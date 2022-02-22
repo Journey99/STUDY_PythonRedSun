3c3d6742294fab98a088546e1dd7f6e6a07c509b
Journey <g48993818@gmail.com>
2022-02-22 15:52:30 +0900

    5week_SW

--- 5week/SW Expert Academy/2805.py
+++ 5week/SW Expert Academy/2805.py
@@ -4,3 +4,29 @@
 https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GLXqKAWYDFAXB
 
 '''
+
+T = int(input())
+
+for t in range(1, T+1):
+    n = int(input())
+
+    farm = [list(map(int, input().split())) for _ in range(n)]
+    mid = n // 2
+    l = mid
+    r = mid
+    sum = 0
+
+    for i in range(n):
+        for j in range(l, r+1):
+            sum += farm[i][j]
+
+        if i <= mid:
+            l -= 1
+            r += 1
+
+        else:
+            l += 1
+            r -= 1
+
+    print(sum)
+
