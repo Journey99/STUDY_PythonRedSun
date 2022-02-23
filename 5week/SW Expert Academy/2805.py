d06c3a0c2f49f912222cc623c93a73b92fbb5d4b
Journey <g48993818@gmail.com>
2022-02-23 21:59:02 +0900

    5week

--- 5week/SW Expert Academy/2805.py
+++ 5week/SW Expert Academy/2805.py
@@ -10,8 +10,8 @@ T = int(input())
 for t in range(1, T+1):
     n = int(input())
 
-    farm = [list(map(int, input().split())) for _ in range(n)]
-    mid = n // 2
+    farm = [list(map(int, input().split())) for _ in range(n)]   # 배열을 만들고
+    mid = n // 2       # 배열의 중앙
     l = mid
     r = mid
     sum = 0
@@ -20,11 +20,11 @@ for t in range(1, T+1):
         for j in range(l, r+1):
             sum += farm[i][j]
 
-        if i <= mid:
+        if i < mid:        # 가운데를 기준으로 위쪽은 양옆을 하나씩 추가
             l -= 1
             r += 1
 
-        else:
+        else:              # 가운데를 기준으로 아래쪽은 양옆을 하나씩 제거
             l += 1
             r -= 1
 
